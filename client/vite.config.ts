56a1122fb7de7a0d1006f4ff345c3ef835c6a342
nekithrill <nemnogoznaal@gmail.com>
2024-09-30 14:06:55 +0300

    added settings for client opening and HMR

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		port: 3000,
+		host: true,
+		watch: {
+			usePolling: true
+		}
+	},
+
 	plugins: [react()],
 	resolve: {
 		alias: {
