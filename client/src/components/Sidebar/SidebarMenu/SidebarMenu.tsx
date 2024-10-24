0ef454f98f332662a0f431c650c4b99b9f2a14d6
nekithrill <nemnogoznaal@gmail.com>
2024-10-24 12:18:18 +0300

    [feat]: added routing

--- client/src/components/Sidebar/SidebarMenu/SidebarMenu.tsx
+++ client/src/components/Sidebar/SidebarMenu/SidebarMenu.tsx
@@ -1,13 +1,25 @@
+import { NavLink } from 'react-router-dom'
+
 export default function SidebarMenu() {
 	return (
 		<>
 			<div className='menu'>
 				<ul>
-					<li>1</li>
-					<li>2</li>
-					<li>3</li>
-					<li>4</li>
-					<li>5</li>
+					<NavLink to='/'>
+						<li>Home</li>
+					</NavLink>
+
+					<NavLink to='/account'>
+						<li>Account</li>
+					</NavLink>
+
+					<NavLink to='/management'>
+						<li>Management</li>
+					</NavLink>
+
+					<NavLink to='/goals'>
+						<li>Savings / Goals</li>
+					</NavLink>
 				</ul>
 			</div>
 		</>
